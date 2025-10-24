db0ffe2786e160177fb00c7a973b9d559ec39ffd
Siva Narayana Muppidi 🌀 <sivanarayanamuppidi11329@gmail.com>
2025-10-25 00:10:13 +0530

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import joblib
+import numpy as np
+
+# Include the create_dashboard function here
+
+if __name__ == "__main__":
+  create_dashboard()
